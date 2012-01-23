1dacbef9f93bafb37f7e9271a887853ebe5b862b
Jeff Dyck <j6dyck@uwaterloo.ca>
2012-01-22 23:34:18 -0500

    Added a copy script to copy over new files

---
+++ copy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Copies the current versions of the files to this dir
+
+cp ~/.vimrc vimrc 
+
+cp ~/.bashrc bashrc 
+
+cp ~/.Xdefaults Xdefaults 
+
+cp ~/.xmonad/xmobar.hs xmobar.hs
+
+cp ~/.xmonad/xmonad.hs xmonad.hs 
+
+cp -r ~/.vim/* vim/ 
