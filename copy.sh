c97908be74a72c11ce2ee2f9c5381fff1314792d
Jeff Dyck <Jeff.C.Dyck@gmail.com>
2012-10-14 17:45:00 -0400

    Removed some unneeded folders, changed copy/distribute

--- copy.sh
+++ copy.sh
@@ -8,8 +8,6 @@ cp ~/.vimrc vimrc
 echo Copying bashrc
 cp ~/.bashrc bashrc 
 
-echo Backing up Sublime Packages
-mv SublimePackages SublimePackages.old
 echo Copying Sublime Packages
 cp -rv ~/.config/sublime-text-2/Packages* SublimePackages
 
@@ -21,9 +19,5 @@ cp ~/.xmonad/xmobar.hs xmobar.hs
 
 echo Copying xmonad.hs
 cp ~/.xmonad/xmonad.hs xmonad.hs 
-
-echo Backing up and copying vim files
-mv vim vim.old
-cp -r ~/.vim/* ./vim
 
 echo Finished!
